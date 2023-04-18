aacac1060766aaacc8185cd7ecce09f86d11dd99
blnd <blnd@MacBook-Air-de-blnd.local>
2023-04-18 14:22:20 +0200

    debuts sur numpy avec creation de tableaux unidimensionnels, et bidimensionnels et petit test jupyter notebook, supprime par la suite

---
+++ librairies/lib_numpy.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# On commence par importer numpy en lui donnant alias np 
+import numpy as np
+
+#Creer un tableau NumPy a une dimension
+
+arr1 = np.array([1, 2, 3, 4, 5])
+
+print(arr1)
+
+#Créer un tableau NumPy à deux dimensions
+arr2 = np.array ([[1, 2, 3 ], [4, 5, 6], [7, 8, 9]])
+
+print(arr2)
+
